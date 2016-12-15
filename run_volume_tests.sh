0b5341476cbb03d821c9c4f356e6f8ae663834b1
myrmecophagaTridactyla <gregorio.ortelli@gmail.com>
2016-12-15 15:47:32 +0100

    Update run_volume_tests.sh

--- run_volume_tests.sh
+++ run_volume_tests.sh
@@ -1,3 +1,7 @@
+echo 'tamandua was here'
 echo 'this is a test' > test.txt
+echo '...and here...'
 mkdir test_directory
+echo 'and here as well...'
 echo 'this is another test' > another_test.txt
+echo 'finally here'
