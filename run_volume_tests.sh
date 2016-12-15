0535f1caab62d8822e3cad5419d884fff9e184e5
myrmecophagaTridactyla <gregorio.ortelli@gmail.com>
2016-12-15 15:48:15 +0100

    Update run_volume_tests.sh

--- run_volume_tests.sh
+++ run_volume_tests.sh
@@ -3,5 +3,6 @@ echo 'this is a test' > test.txt
 echo '...and here...'
 mkdir test_directory
 echo 'and here as well...'
+cd test_directory
 echo 'this is another test' > another_test.txt
 echo 'finally here'
