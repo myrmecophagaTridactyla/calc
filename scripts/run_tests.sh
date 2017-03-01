2ff8bb42a7b80b03a48dd1b2f195b27a35849e8f
myrmecophagaTridactyla <gregorio.ortelli@gmail.com>
2017-03-01 10:02:48 +0100

    Update run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo ">>>>,======,*=~~ ... . .. .. ... .. . . .. ..^^.. . .. ."
-
-cd /source/client && npm install && bower install --allow-root --config.interactive=false -s
-
-echo "           >> --- NPM INSTALL AND BOWER INSTALL DONE --- <<"
-
 cd /source
 
 sbt test
